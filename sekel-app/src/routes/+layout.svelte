397e115d6874b3aa80c6273c93ee3b16db1098d0
dh 2407 <medalihachicha@proton.me>
2024-06-29 18:12:26 +0000

    clean

--- sekel-app/src/routes/+layout.svelte
+++ sekel-app/src/routes/+layout.svelte
@@ -1,11 +1,5 @@
-<slot></slot><script>import "../app.css";</script>
+<script>
+  import "../app.css";
+</script>
 
-<style>
-
-
-
-  /* Example: Apply Inria Sans as the default font */
-  body {
-    font-family: 'Inria Sans', sans-serif;
-  } 
-</style>
+<slot></slot>
