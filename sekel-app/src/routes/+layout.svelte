9f99d145878d120a57383e8af753f111c59f0911
atsamw <atsamw@proton.me>
2024-06-29 19:29:04 +0200

    init_webapp_svelt

---
+++ sekel-app/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot></slot><script>import "../app.css";</script>
+
+<style>
+
+
+
+  /* Example: Apply Inria Sans as the default font */
+  body {
+    font-family: 'Inria Sans', sans-serif;
+  } 
+</style>
