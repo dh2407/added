4ce9aab383fe7c7badd14b8c8a575c147407a09a
dh 2407 <medalihachicha@proton.me>
2024-07-13 15:35:17 +0000

    init capacitor

---
+++ web-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.added',
+  appName: 'web-app',
+  webDir: 'dist'
+};
+
+export default config;
